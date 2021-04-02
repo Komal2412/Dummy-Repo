486905d74b57ca7204402aadbc36087c237c0600
komalpreet24 <kaur242k@gmail.com>
2021-04-02 17:58:15 +0530

    Initial commit Navigation

---
+++ app/src/main/java/com/example/dogs/view/ListFragment.kt
@@ -0,0 +1,42 @@
+package com.example.dogs.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import com.example.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
+
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+
+    // so that elements are created before we put actions on them
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonDetails.setOnClickListener {
+
+            // ListFragmentDirections is a generated class bc we used fragments in nav graph
+            val action = ListFragmentDirections.actionDetailFragment()
+            action.dogUuid = 5
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
+
+
+}
